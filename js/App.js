d8e15260dda2f65fcb4aaa3faeaee3952c13d8f8
Matt Nam <namseunghun97@gmail.com>
2022-11-16 23:02:47 +0900

    react

---
+++ js/App.js
@@ -0,0 +1,7 @@
+import Spline from '@splinetool/react-spline';
+
+export default function App() {
+  return (
+    <Spline scene="https://prod.spline.design/dbq0kYVnft9-Jkrw/scene.splinecode" />
+  );
+}
